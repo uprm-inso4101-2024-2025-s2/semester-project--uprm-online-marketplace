a13b8032132cce512a3d5977b9c97e31fae776a7
cristian.ramos <179621819+cristianramos9@users.noreply.github.com>
2025-04-11 17:27:47 -0400

    [UI] Added String constants
    Summary:
    - Added constant Strings in new directory.
    
    Changes:
    - Added directory for constants 'constants/'.
    - Added file with String constants 'text.dart'.
    
    Why:
    - Now there is a directory that will contain all constants needed.
    - Now, when there is a need to use the specific string "Email", only
      needs to import the file with constant file for strings and call the
      specific variable for the desired string.
    - This will reduce the need to type some strings in the code, and reuse
      variables with strings.
    - Other constants can be added, for example: 'size.dart' which will
      containt variables for different sizes needes through the code.
    - Example of size variable: 'const int fieldHeight = 40' for use on
      specific text form fields.
    - Using variables instead of a String or a number directly in the code
      improves readibility of the code.

---
+++ lib/constants/text.dart
@@ -0,0 +1,13 @@
+/* -- Text String Variables -- */
+
+// Titles
+const String t_SignIn = "Sign In";
+
+// Forms
+const String t_Email = "Email";
+const String t_Password = "Password";
+
+// Buttons
+const String t_ForgotPassword = "Forgot Password";
+const String t_SignInBtn = "SIGN IN";
+const String t_SignUpBtn = "SIGN UP";
