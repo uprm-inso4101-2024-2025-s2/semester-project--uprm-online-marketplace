2c24e4ccdd633fa1c4b0d1ff26e74f27a21baedb
cristian.ramos <179621819+cristianramos9@users.noreply.github.com>
2025-04-11 16:54:28 -0400

    [UI] Added login page rewritten
    Summary:
    - The login page was rewritten to clean its code and import widgets.
    
    Changes:
    - Added login page 'sign_in_page.dart'.
    - Page was added in a separate directory.
    - Deleted empty file in 'pages/'.
    
    Why:
    - The login page was rewritten. It import widgets from other files and is easier to read and maintain.

---
+++ lib/pages/login/sign_in_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:semesterprojectuprmonlinemarketplace/src/constants/text.dart';
+import 'package:semesterprojectuprmonlinemarketplace/src/login/login_widgets/buttons.dart';
+import 'package:semesterprojectuprmonlinemarketplace/src/login/login_widgets/text_fields.dart';
+
+void main() {
+  /*
+  * The purpose of this main function is to test the class
+  * below SignInPage without depending on other components
+  * of the application
+  */
+
+  // Entry point to Sign In Page
+  runApp(MaterialApp(home: SignInPage()));
+}
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(t_SignIn),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          EmailField(),
+          PasswordField(),
+          ForgotPasswordBtn(),
+          SignInBtn(),
+        ],
+      ),
+    );
+  }
+}
