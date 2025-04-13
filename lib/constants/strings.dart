306fd4cd45ec213d09ff24c2322e80a82cf8bf75
cristian.ramos <179621819+cristianramos9@users.noreply.github.com>
2025-04-13 17:54:10 -0400

    [Refactor] Basic rewrite of login page
    Summary:
    - Login page is rewritten in 'lib/pages/new_login_page.dart'.
    - Empty files removed.
    - Added constant file for strings.
    
    Changes:
    - Added new file 'new_login_page.dart'.
    - Removed empty files in 'lib/constants' and 'lib/pages'.
    - Added new file 'strings.dart'.
    
    Why:
    - To understand better the code and rearrange the UI code, the login page
      is rewritten.
    - The login page from the integration branch is used as base reference,
      while also using other login pages in the project as reference too.
    - The goal is to have the login page compatible with the login page from
      the integration branch and minimize the conflicts in a merge.
    - In addition, the login file that was supposed to be used was not used,
      so this commit start to use the correct login page while adapting it
    to use the conventions set by the project team.
    - The constant file for strings will allow to use variables instead of
      strings in the code. This will improve readibility of the code.

---
+++ lib/constants/strings.dart
@@ -0,0 +1,16 @@
+/* All strings to be used. */
+
+/* -- login_page.dart -- */
+
+
+/* -- AUTHENTICATION STRINGS -- */
+
+const String loginAppBarBack = "Back";
+const String loginAppBarTitle = "Log in to your account";
+
+const String loginEmail = "Email";
+const String loginPassword = "Password";
+
+const String loginForgotPassword = "Forgot Password";
+
+const String loginButton = "SIGN IN";
---
+++ lib/pages/new_login_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+import '../components/my_button.dart';
+import '../components/my_textfield.dart';
+import '../constants/strings.dart';
+
+void main() {
+  /*
+   * Entry point to the Login Page.
+   * The purpose of this main function is to test this file
+   * separately from the rest of the application.
+   */
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: LoginPage(),
+    ),
+  );
+}
+
+class LoginPage extends StatelessWidget {
+  /*
+   * To use the Login Page call this class from any file.
+   */
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: TextButton(
+          onPressed: () {},
+          child: Text(loginAppBarBack),
+        ),
+        title: const Text(loginAppBarTitle),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: LoginBody(),
+    );
+  }
+}
+
+/* --------------------------------------------------------------
+ * All classes below this comment shall not be called from
+ * other files. ONLY the LoginPage class above can call them.
+ */
+class LoginBody extends StatefulWidget {
+  // Start LoginBody
+  const LoginBody({super.key});
+
+  @override
+  State<LoginBody> createState() => _LoginBodyState();
+}
+
+class _LoginBodyState extends State<LoginBody> {
+  static const _space10 = const SizedBox(height: 10);
+  static const _space20 = const SizedBox(height: 20);
+  static const _space25 = const SizedBox(height: 25);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Page Title
+            Text("Title: Login"),
+            _space20,
+            // Email Field
+            MyTextfield(
+              hintText: loginEmail,
+              obscureText: false,
+              controller: TextEditingController(),
+            ),
+            _space10,
+            MyTextfield(
+              hintText: loginPassword,
+              obscureText: true,
+              controller: TextEditingController(),
+            ),
+            _space20,
+            TextButton(
+              onPressed: () {},
+              child: Text(loginForgotPassword),
+            ),
+            _space20,
+            MyButton(text: loginButton, onTap: () {}),
+            _space25,
+          ],
+        ),
+      ),
+    );
+  }
+} // End LoginBody
