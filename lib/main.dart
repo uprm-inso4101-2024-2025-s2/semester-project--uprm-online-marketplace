8522a9e33b4fbd800c7827bc1b9734e5d3604d0f
cristian.ramos <179621819+cristianramos9@users.noreply.github.com>
2025-04-16 21:46:30 -0400

    [Backend] Added Firebase initialization code
    Summary:
    - Added code to initialize Firebase by uncommenting already existing
      code in the file.
    
    Changes:
    - Uncommented Firebase initialization code.
    - Some code formatting was changed automatically.
    
    Why:
    - Firebase needs to be initialized when the application is started, and
      must occur in main function of 'main.dart'.
    - There is a setting in Android Studio that automatically formats the
      code for easier reading when text reaches certain column.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:semesterprojectuprmonlinemarketplace/providers/notification_provider.dart';
 
 import 'Router/router.dart';
+import 'firebase_options.dart';
 import 'themes/uprm_green.dart';
 
 void main() async {
   /* --- Initialize Firebase --- */
-  // initializeFirebase();
+  initializeFirebase();
 
   /* --- Run Application --- */
   runApp(
@@ -21,29 +26,29 @@ void main() async {
 }
 
 /* --- Method to initialize Firebase --- */
-// Future<void> initializeFirebase() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//
-//   if (kDebugMode) {
-//     // Only use emulators in debug mode
-//     try {
-//       FirebaseFirestore.instance.useFirestoreEmulator(
-//         'localhost',
-//         8080,
-//       );
-//       FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-//     } catch (e) {
-//       print("Firebase Emulator Error: $e");
-//     }
-//   }
-//   // Uncomment to see ports of the emulator
-//   // print(
-//   //   "Firebase Emulators Connected: Firestore (8080), Auth (9099)",
-//   // );
-// }
+Future<void> initializeFirebase() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  if (kDebugMode) {
+    // Only use emulators in debug mode
+    try {
+      FirebaseFirestore.instance.useFirestoreEmulator(
+        'localhost',
+        8080,
+      );
+      FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    } catch (e) {
+      print("Firebase Emulator Error: $e");
+    }
+  }
+  //   // Uncomment to see ports of the emulator
+  //   // print(
+  //   //   "Firebase Emulators Connected: Firestore (8080), Auth (9099)",
+  //   // );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
